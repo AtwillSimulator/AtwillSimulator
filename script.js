f397784674e2122035ddb2ad8eed3702152ba0fa
landsdown13 <13hazzas@gmail.com>
2026-09-16 16:21:46 +1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let username;
+const quotes = ["That's powerful", "That's a shame"];
+
+
+document.getElementById("ATH1").textContent = "Atwill Simulator";
+document.getElementById("ATP").textContent = "WARNING: This webpage is not fully functional yet.";
+document.getElementById("Name").textContent = "Username: ";
+document.getElementById("AtwillQuote").textContent = "Mr Atwill"
+document.getElementById("AtwillImage").width = "200";
+
+document.getElementById("UsernameButton").onclick = function(){
+    username = document.getElementById("Text1").value;
+    if (username == "lansdown") {
+        window.alert("ERROR: Username unavaliable")
+    } else {
+        if (username == "landsdown") {
+            window.alert("ERROR: Username unavaliable")
+        } else {
+            document.getElementById("Name").textContent = "Username: " + username;
+        } 
+    }
+}
+document.getElementById("QuoteButton").onclick = function(){
+    document.getElementById("AtwillQuote").textContent = `"` + quotes[Math.floor(Math.random() * quotes.length)] + `"` + `\n- Mr Atwill`;
+}
+
+/*
+Atwill Simulator, let's go
+Atwill Simulator is a webpage designed and based on Mr. Thomas Atwill
+*/
